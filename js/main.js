e53fa695e837b86460130bf322db7913849ce2be
naumannfl79974 <naumannfl79974@th-nuernberg.de>
2020-02-26 14:18:35 +0100

    Transition of remove and adding symbol
    Signs for adding have shapes which change when hovered over and out. There also transition in changing the shapes. Now adder box lightens a bit when in focus.

--- js/main.js
+++ js/main.js
@@ -14,23 +14,54 @@
         let toDoBoxRemovable = ToDoBoxRemovable(text);
         list[0].appendChild(toDoBoxRemovable);
         textarea.value = "";
-      } else {
-        console.log("asdfsa");
       }
     },
     false
   );
+  adderSign.addEventListener(
+    "mouseover",
+    function(event) {
+      let leftBar = event.currentTarget.querySelector(".leftBar");
+      let rightBar = event.currentTarget.querySelector(".rightBar");
+      leftBar.classList.add("hover");
+      rightBar.classList.add("hover");
+    },
+    false
+  );
+  adderSign.addEventListener(
+    "mouseout",
+    function(event) {
+      let leftBar = event.currentTarget.querySelector(".leftBar");
+      let rightBar = event.currentTarget.querySelector(".rightBar");
+      leftBar.classList.remove("hover");
+      rightBar.classList.remove("hover");
+    },
+    false
+  );
+
   // Testing.
   let testString = "xxxx".repeat(80);
   list[0].appendChild(ToDoBoxRemovable(testString));
+  // EndTesting.
   // Returns a ToDoBox with text and a sign to click on. Clicking on that removes the toDoBox.
 
   function ToDoBoxRemovable(text) {
     let toDoBoxRemovable = document.createElement("div");
     toDoBoxRemovable.classList.add("toToDoBoxRemovable");
+
     let removable = document.createElement("div");
     removable.classList.add("removable");
     removable.addEventListener("click", callbackClick, false);
+    removable.addEventListener("mouseover", callbackHoverIN, false);
+    removable.addEventListener("mouseout", callbackHoverOut, false);
+
+    let leftBar = document.createElement("div");
+    leftBar.classList.add("leftBar");
+    let rightBar = document.createElement("div");
+    rightBar.classList.add("rightBar");
+    removable.appendChild(leftBar);
+    removable.appendChild(rightBar);
+
     let line = document.createElement("p");
     line.classList.add("line");
     line.innerText = text;
@@ -40,6 +71,20 @@
 
     function callbackClick(event) {
       event.currentTarget.parentNode.remove();
+    }
+
+    function callbackHoverIN(event) {
+      let leftBar = event.currentTarget.querySelector(".leftBar");
+      let rightBar = event.currentTarget.querySelector(".rightBar");
+      leftBar.classList.add("hover");
+      rightBar.classList.add("hover");
+    }
+
+    function callbackHoverOut(event) {
+      let leftBar = event.currentTarget.querySelector(".leftBar");
+      let rightBar = event.currentTarget.querySelector(".rightBar");
+      leftBar.classList.remove("hover");
+      rightBar.classList.remove("hover");
     }
   }
 })();
